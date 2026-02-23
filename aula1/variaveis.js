9efecf3ed061537b31651c81ad6a41cd532aae04
Otavio <jusileufaleceu@gmail.com>
2026-02-23 08:45:18 -0300

    Add files via upload

---
+++ aula1/variaveis.js
@@ -0,0 +1,26 @@
+var nomedavariavel = "valor da variavel";
+var outravariavel = 'valor da outra variavel';
+var variavelidade = 22;
+var variaveltenhocabelobranco = false;
+var variavelindefinida;
+
+console.log(nomedavariavel);
+console.log(outravariavel);
+console.log(variavelidade);
+console.log(variaveltenhocabelobranco);
+console.log(variavelindefinida);
+
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+//neste caso tr irá contar o valor true e fl o valor false
+var int1 = tr + 1;
+var int2 = fl + 1;
+
+console.log(a);
+console.log(b);
+console.log(tr);
+console.log(fl);
+console.log(int1);
+console.log(int2);
