faf6ad8125fa7a3255eee0390d4bbeef8343a593
otavio-sanches <jusileufaleceu@gmail.com>
2026-03-30 10:49:22 -0300

    aula05

---
+++ aula05/index.js
@@ -0,0 +1,10 @@
+function retorno(){
+    var num1 = prompt ("Escolha seu número: ");
+    var num2 = prompt ("Escolha outro número: ");
+    if (num1 > num2) {
+        alert("o número 1 é maior") 
+    } else {
+        alert("o número 2 é maior")
+    }
+}
+console.log(retorno)
