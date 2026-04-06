8799eca4e706dd31818260fe91bf984b27baf0f4
otavio-sanches <jusileufaleceu@gmail.com>
2026-04-06 09:14:10 -0300

    aaaaa

--- aula05/index.js
+++ aula05/index.js
@@ -1,10 +1,59 @@
-function retorno(){
-    var num1 = prompt ("Escolha seu número: ");
-    var num2 = prompt ("Escolha outro número: ");
-    if (num1 > num2) {
-        alert("o número 1 é maior") 
-    } else {
-        alert("o número 2 é maior")
-    }
+//Exercicio A
+function maior(a, b) {
+    return a > b ? a : b;
 }
-console.log(retorno)
+
+let n1 = Number(prompt("Digite o primeiro número:"));
+let n2 = Number(prompt("Digite o segundo número:"));
+
+console.log("Maior número:", maior(n1, n2));
+
+//Exercicio B
+function ePar(n) {
+    return n % 2 === 0;
+}
+
+let num = Number(prompt("Digite um número:"));
+
+console.log("É par?", ePar(num));
+
+//Exercicio C
+function fatorial(n) {
+    let resultado = 1;
+    for (let i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+let num1 = Number(prompt("Digite um número:"));
+
+console.log("Fatorial:", fatorial(num1));
+
+//Exercicio D
+function media(lista) {
+    let soma = 0;
+    for (let i = 0; i < lista.length; i++) {
+        soma += lista[i];
+    }
+    return soma / lista.length;
+}
+
+let quantidade = Number(prompt("Quantos números você quer digitar?"));
+let numeros = [];
+
+for (let i = 0; i < quantidade; i++) {
+    let n = Number(prompt(`Digite o ${i + 1}º número:`));
+    numeros.push(n);
+}
+
+console.log("Média:", media(numeros));
+
+//Exercicio E
+function celsiusParaFahrenheit(c) {
+    return (c * 9/5) + 32;
+}
+
+let temp = Number(prompt("Digite a temperatura em Celsius:"));
+
+console.log("Temperatura em Fahrenheit:", celsiusParaFahrenheit(temp));
